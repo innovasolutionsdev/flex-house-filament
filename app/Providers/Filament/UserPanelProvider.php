e27c73f0d2a8bc267d34fd81408f5e1810f7e9e5
cb009917 <cb009917@students.apiit.lk>
2024-10-14 17:13:46 +0530

    user work out progress tracking

--- app/Providers/Filament/UserPanelProvider.php
+++ app/Providers/Filament/UserPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\User\Widgets\WeeklyExerciseProgressWidget;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -38,6 +39,7 @@ class UserPanelProvider extends PanelProvider
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
+                WeeklyExerciseProgressWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
