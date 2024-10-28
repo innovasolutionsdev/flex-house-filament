15ff85a51b59524bef4843ccf3542ab5f90c921d
cb009917 <cb009917@students.apiit.lk>
2024-10-28 15:37:44 +0530

    Update CreateWorkoutLog.php

--- app/Filament/User/Resources/WorkoutLogResource/Pages/CreateWorkoutLog.php
+++ app/Filament/User/Resources/WorkoutLogResource/Pages/CreateWorkoutLog.php
@@ -14,30 +14,6 @@ class CreateWorkoutLog extends CreateRecord
     protected static string $resource = WorkoutLogResource::class;
 
 
-    public function afterCreate(Request $request): void
-    {
-        // Validate and create the workout log
-        $workoutLog = WorkoutLog::create($request->validated());
 
-        // Loop through workoutLogDetails to calculate and store volume
-        foreach ($request->input('workoutLogDetails') as $detail) {
-            $sets = $detail['sets']; // Get the sets for the exercise
-            $exerciseId = $detail['exercise_id'];
-
-            $totalVolume = 0;
-
-            // Calculate total volume for this exercise
-            foreach ($sets as $set) {
-                $totalVolume += $set['reps'] * $set['weight'];
-            }
-
-            // Store the calculated volume in the exercise_volumes table
-            Exercise_Volume::create([
-                'exercise_id' => $exerciseId,
-                'volume' => $totalVolume,
-            ]);
-        }
-
-
-    }
+ 
 }
