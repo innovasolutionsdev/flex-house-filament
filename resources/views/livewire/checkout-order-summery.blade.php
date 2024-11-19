998056a433ff528410344b2e32cc740c7d27003a
g1hantharuka <gihantharuka2499@gmail.com>
2024-11-19 10:42:03 +0530

    Dark mode

--- resources/views/livewire/checkout-order-summery.blade.php
+++ resources/views/livewire/checkout-order-summery.blade.php
@@ -1,18 +1,18 @@
-<div class="bg-white p-6 rounded-lg shadow-md h-96 overflow-y-auto">
-    <h2 class="text-xl font-semibold mb-4">
+<div class="bg-white dark:bg-[#141414] p-6 rounded-lg shadow-md h-96 overflow-y-auto">
+    <h2 class="text-xl font-semibold mb-4 dark:text-white">
         Order Summary
     </h2>
     @foreach($cartItems as $item)
         <div class="flex items-center mb-4 p-4 border-b last:border-b-0">
             <img alt="{{ $item['name'] }}" class="w-16 h-16 rounded-md" height="60" src="{{ $item['image'] }}" width="60"/>
             <div class="ml-4 flex-1">
-                <p class="text-sm font-medium text-gray-700 truncate">
+                <p class="text-sm font-medium text-gray-700 dark:text-gray-300 truncate">
                     {{ $item['name'] }}
                 </p>
-                <p class="text-sm text-gray-500">
+                <p class="text-sm text-gray-500 dark:text-gray-300">
                     Qty: {{ $item['qty'] }}
                 </p>
-                <p class="text-sm font-medium text-gray-900">
+                <p class="text-sm font-medium text-gray-900 dark:text-white">
                     රු.{{ $item['total'] }}
                 </p>
             </div>
