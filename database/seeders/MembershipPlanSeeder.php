358130ece2affd00df9335af175fcf1ef5854966
cb009917 <cb009917@students.apiit.lk>
2025-03-12 14:14:50 +0530

    fixing seeder files

--- database/seeders/MembershipPlanSeeder.php
+++ database/seeders/MembershipPlanSeeder.php
@@ -15,6 +15,8 @@ class MembershipPlanSeeder extends Seeder
     {
         MembershipPlan::create([
             'name' => 'Student',
+            'discount_price' => 5000,
+            'discount' => 1,
             'price' => 7500,
             'duration' => 30, // 30 days
             'description' => 'Perfect for students looking to stay fit for a short period.',
@@ -22,6 +24,8 @@ class MembershipPlanSeeder extends Seeder
 
         MembershipPlan::create([
             'name' => 'Standard Plan',
+            'discount_price' => 10000,
+            'discount' => 1,
             'price' => 12000,
             'duration' => 60, // 60 days
             'description' => 'Ideal for individuals who want a flexible two-month fitness plan.',
@@ -29,6 +33,8 @@ class MembershipPlanSeeder extends Seeder
 
         MembershipPlan::create([
             'name' => '3 Months',
+            'discount_price' => 15000,
+            'discount' => 0,
             'price' => 18000,
             'duration' => 90, // 90 days
             'description' => 'A three-month plan for those committed to a medium-term fitness goal.',
@@ -36,6 +42,8 @@ class MembershipPlanSeeder extends Seeder
 
         MembershipPlan::create([
             'name' => 'Annual',
+            'discount_price' => 30000,
+            'discount' => 0,
             'price' => 32000,
             'duration' => 365, // 365 days
             'description' => 'Best value for those who want to stay dedicated all year round.',
@@ -43,6 +51,8 @@ class MembershipPlanSeeder extends Seeder
 
         MembershipPlan::create([
             'name' => '6 Months',
+            'discount_price' => 20000,
+            'discount' => 1,
             'price' => 23000,
             'duration' => 180, // 180 days
             'description' => 'Great option for half-year fitness enthusiasts.',
