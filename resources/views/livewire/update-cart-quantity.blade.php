2c31c0f861d5d466aee7a4c7676907cdfa35deae
cb009917 <cb009917@students.apiit.lk>
2025-03-27 14:51:21 +0530

    Update update-cart-quantity.blade.php

--- resources/views/livewire/update-cart-quantity.blade.php
+++ resources/views/livewire/update-cart-quantity.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Quantity Input -->
     <input type="text" wire:model.lazy="quantity" wire:change="updateCartQuantity"
-           class="w-8 h-7 text-center border-t border-b border-gray-300" type="text" value="1"
+           class="w-12 h-7 text-center border-t border-b border-gray-300" type="text" value="1"
            readonly />
 
     <!-- Increment Button -->
