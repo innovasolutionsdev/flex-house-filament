76223aaaa41d3d2aa2be45ac6d5cf27cb38994c0
cb009917 <cb009917@students.apiit.lk>
2025-03-27 21:29:15 +0530

    updates

--- resources/views/livewire/checkout-order-summery.blade.php
+++ resources/views/livewire/checkout-order-summery.blade.php
@@ -4,8 +4,8 @@
     </h2>
     @foreach($cartItems as $item)
         <div class="flex items-center mb-4 p-4 last:border-b-0">
-            {{-- <img alt="{{ $item['name'] }}" class="w-16 h-16 rounded-md" height="60" src="{{ $item['image'] }}" width="60"/> --}}
-            <img alt="{{ $item['name'] }}" class="w-16 h-16 rounded-md" height="60" src="{{ asset('img/prod.jpg') }}" width="60"/>
+             <img alt="{{ $item['name'] }}" class="w-16 h-16 rounded-md" height="60" src="{{ $item['image'] }}" width="60"/>
+{{--            <img alt="{{ $item['name'] }}" class="w-16 h-16 rounded-md" height="60" src="{{ asset('img/prod.jpg') }}" width="60"/>--}}
             <div class="ml-4 flex justify-between w-full">
 
                 <div>
