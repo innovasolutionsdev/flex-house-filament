6f373e58f19c2f2639a276550d974a26f696e4bf
Gihan Tharuka <141792890+gihan-tharuka@users.noreply.github.com>
2025-04-05 19:49:35 +0530

    pricing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -163,7 +163,7 @@
 {{--                                <img alt="{{ $value->name }}" class="rounded-lg h-50 md:h-72 object-cover" src="{{asset('img/prod.jpg')}}" />--}}
 
                             </a>
-                            {{-- @dd($value->getFirstMediaUrl('product_image')) --}}
+                     
 
                             @if ($value->on_sale)
                                 <span class="absolute top-2 right-2 bg-red-500 text-white px-2 py-1 text-xs rounded-lg">Sale!</span>
@@ -217,6 +217,62 @@
             </div>
         </div>
     </div>
+
+    {{-- membership plans --}}
+    <div id="pricing" class="w-full dark:bg-[#141414] py-12 text-center px-4 pt-8">
+        <div class="text-center mt-20">
+            <h2 class="text-[#F41E1E] text-lg font-bold uppercase mb-2 flex items-center justify-center">
+                <span class="inline-block w-12 h-0.5 bg-[#F41E1E] mx-2">
+                </span>
+                Membership plans
+                <span class="inline-block w-12 h-0.5 bg-[#F41E1E] mx-2">
+                </span>
+            </h2>
+            <h1 class="text-4xl font-extrabold mb-12 text-gray-900 dark:text-white">
+                Best Plans For You
+            </h1>
+            <div class="flex overflow-x-auto space-x-4 px-4 pl-16 sm:ml-2 scrollbar-hide">
+
+                @foreach($plans as $plan)
+                <!-- Card 1 -->
+                <div
+                    class="bg-white dark:bg-[#171717] rounded-lg shadow-lg transform transition duration-500 hover:scale-105 w-72 relative m-4 flex-shrink-0">
+                    <img alt="Gym facilities image" class="rounded-t-lg" height="160"
+                        src="{{$plan->getFirstMediaUrl('membership_thumbnail') }}"
+                        width="288" />
+                    <div
+                        class="absolute top-0 right-0 bg-[#F41E1E] text-white text-xs font-bold py-1 px-2 rounded-bl-lg">
+                        Valid till only
+                    </div>
+                    <div class="bg-white text-gray-800 text-center py-1">
+                        <h2 class="text-lg font-bold ">
+                            {{$plan->name}}
+                        </h2>
+                    </div>
+                    <div class="text-center mt-2 px-4">
+                        @if($plan->discount == 1)
+                        <p class="text-gray-600 dark:text-gray-300 text-xl line-through">
+                            {{$plan->discount_price}}
+                        </p>
+                        @endif
+                        <p class="text-[#F41E1E] text-2xl font-extrabold">
+                            Rs.{{$plan->price}}
+                        </p>
+                        <p class="mt-2 text-gray-600 dark:text-gray-400">
+                            {{$plan->description}}
+                        </p>
+                        <button
+                            class="mt-4 mb-4 bg-[#141414] dark:bg-[#F41E1E] text-white py-2 px-4 font-bold rounded-full shadow-md hover:bg-[#F41E1E] transition duration-300"
+                            onclick="window.location.href='{{ url('register') }}'">
+                            Register Now
+                        </button>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    {{-- membership plans end --}}
 
     {{-- Our servces --}}
     <div id="services" class="w-full dark:bg-[#141414] py-12 px-4">
@@ -398,59 +454,7 @@
 
     {{-- proccess steps end --}}
 
-    <div id="pricing" class="w-full dark:bg-[#141414] py-12 text-center px-4 pt-8">
-        <div class="text-center mt-20">
-            <h2 class="text-[#F41E1E] text-lg font-bold uppercase mb-2 flex items-center justify-center">
-                <span class="inline-block w-12 h-0.5 bg-[#F41E1E] mx-2">
-                </span>
-                Membership plans
-                <span class="inline-block w-12 h-0.5 bg-[#F41E1E] mx-2">
-                </span>
-            </h2>
-            <h1 class="text-4xl font-extrabold mb-12 text-gray-900 dark:text-white">
-                Best Plans For You
-            </h1>
-            <div class="flex overflow-x-auto space-x-4 px-4 pl-16 sm:ml-2 scrollbar-hide">
 
-                @foreach($plans as $plan)
-                <!-- Card 1 -->
-                <div
-                    class="bg-white dark:bg-[#171717] rounded-lg shadow-lg transform transition duration-500 hover:scale-105 w-72 relative m-4 flex-shrink-0">
-                    <img alt="Gym facilities image" class="rounded-t-lg" height="160"
-                        src="{{$plan->getFirstMediaUrl('membership_thumbnail') }}"
-                        width="288" />
-                    <div
-                        class="absolute top-0 right-0 bg-[#F41E1E] text-white text-xs font-bold py-1 px-2 rounded-bl-lg">
-                        Valid till only
-                    </div>
-                    <div class="bg-white text-gray-800 text-center py-1">
-                        <h2 class="text-lg font-bold ">
-                            {{$plan->name}}
-                        </h2>
-                    </div>
-                    <div class="text-center mt-2 px-4">
-                        @if($plan->discount == 1)
-                        <p class="text-gray-600 dark:text-gray-300 text-xl line-through">
-                            {{$plan->discount_price}}
-                        </p>
-                        @endif
-                        <p class="text-[#F41E1E] text-2xl font-extrabold">
-                            Rs.{{$plan->price}}
-                        </p>
-                        <p class="mt-2 text-gray-600 dark:text-gray-400">
-                            {{$plan->description}}
-                        </p>
-                        <button
-                            class="mt-4 mb-4 bg-[#141414] dark:bg-[#F41E1E] text-white py-2 px-4 font-bold rounded-full shadow-md hover:bg-[#F41E1E] transition duration-300"
-                            onclick="window.location.href='{{ url('register') }}'">
-                            Register Now
-                        </button>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
 
 
     <style>
--- resources/views/pages/pricing.blade.php
+++ resources/views/pages/pricing.blade.php
@@ -1,70 +1,59 @@
 <x-app-layout>
-    <div id="pricing" class="w-full dark:bg-[#141414] py-12 text-center px-4 pt-8">
-        <div class="text-center mt-20">
-            <h2 class="text-[#F41E1E] text-lg font-bold uppercase mb-2 flex items-center justify-center">
-                <span class="inline-block w-12 h-0.5 bg-[#F41E1E] mx-2">
-                </span>
-                Membership plans
-                <span class="inline-block w-12 h-0.5 bg-[#F41E1E] mx-2">
-                </span>
-            </h2>
-            <h1 class="text-4xl font-extrabold mb-12 text-gray-900 dark:text-white">
-                Best Plans For You
-            </h1>
-            <div class="flex overflow-x-auto space-x-4 px-4 pl-16 sm:ml-2 scrollbar-hide">
 
-                @foreach($plans as $plan)
+
+        {{-- membership plans --}}
+        <div id="pricing" class="w-full dark:bg-[#141414] py-12 text-center px-4 pt-2">
+            <div class="text-center mt-4">
+                {{-- <h2 class="text-[#F41E1E] text-lg font-bold uppercase mb-2 flex items-center justify-center">
+                    <span class="inline-block w-12 h-0.5 bg-[#F41E1E] mx-2">
+                    </span>
+                    Membership plans
+                    <span class="inline-block w-12 h-0.5 bg-[#F41E1E] mx-2">
+                    </span>
+                </h2>
+                <h1 class="text-4xl font-extrabold mb-12 text-gray-900 dark:text-white">
+                    Best Plans For You
+                </h1> --}}
+                <div class="flex overflow-x-auto space-x-4 px-4 pl-16 sm:ml-2 scrollbar-hide">
+    
+                    @foreach($plans as $plan)
                     <!-- Card 1 -->
                     <div
                         class="bg-white dark:bg-[#171717] rounded-lg shadow-lg transform transition duration-500 hover:scale-105 w-72 relative m-4 flex-shrink-0">
                         <img alt="Gym facilities image" class="rounded-t-lg" height="160"
-                             src="{{$plan->getFirstMediaUrl('membership_thumbnail') }}"
-                             width="288" />
+                            src="{{$plan->getFirstMediaUrl('membership_thumbnail') }}"
+                            width="288" />
                         <div
                             class="absolute top-0 right-0 bg-[#F41E1E] text-white text-xs font-bold py-1 px-2 rounded-bl-lg">
                             Valid till only
                         </div>
-                        <div class="bg-gray-800 text-white text-center py-4">
-                            <h2 class="text-xl font-bold">
+                        <div class="bg-white text-gray-800 text-center py-1">
+                            <h2 class="text-lg font-bold ">
                                 {{$plan->name}}
                             </h2>
                         </div>
-                        <div class="text-center mt-4 px-6">
+                        <div class="text-center mt-2 px-4">
                             @if($plan->discount == 1)
-                                <p class="text-gray-600 dark:text-gray-300 text-2xl line-through">
-                                    {{$plan->discount_price}}
-                                </p>
+                            <p class="text-gray-600 dark:text-gray-300 text-xl line-through">
+                                {{$plan->discount_price}}
+                            </p>
                             @endif
-                            <p class="text-[#F41E1E] text-4xl font-extrabold">
+                            <p class="text-[#F41E1E] text-2xl font-extrabold">
                                 Rs.{{$plan->price}}
                             </p>
-                            <p class="mt-4 text-gray-600 dark:text-gray-400">
+                            <p class="mt-2 text-gray-600 dark:text-gray-400">
                                 {{$plan->description}}
                             </p>
                             <button
-                                class="mt-6 mb-2 bg-[#141414] dark:bg-[#F41E1E] text-white py-2 px-6 font-bold rounded-md shadow-md hover:bg-[#F41E1E] transition duration-300">
+                                class="mt-4 mb-4 bg-[#141414] dark:bg-[#F41E1E] text-white py-2 px-4 font-bold rounded-full shadow-md hover:bg-[#F41E1E] transition duration-300"
+                                onclick="window.location.href='{{ url('register') }}'">
                                 Register Now
                             </button>
                         </div>
                     </div>
-                @endforeach
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
-
-
-    <style>
-        /* Hide scrollbar */
-        .scrollbar-hide::-webkit-scrollbar {
-            display: none;
-        }
-
-        .scrollbar-hide {
-            -ms-overflow-style: none;
-            /* IE and Edge */
-            scrollbar-width: none;
-            /* Firefox */
-        }
-    </style>
-    <!-- Membership Section End -->
+        {{-- membership plans end --}}
 </x-app-layout>
