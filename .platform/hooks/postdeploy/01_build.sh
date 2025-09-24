ab2aad501d9cb086a085ea29d0528a04ed77c7e8
Nimesh Rajapakse <nimeshraja18@gmail.com>
2025-09-24 20:20:24 +0530

    hg

--- .platform/hooks/postdeploy/01_build.sh
+++ .platform/hooks/postdeploy/01_build.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-# Install PHP deps
-composer install --no-dev --optimize-autoloader
+# #!/bin/bash
+# # Install PHP deps
+# composer install --no-dev --optimize-autoloader
 
-# Laravel optimizations
-php artisan config:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+# # Laravel optimizations
+# php artisan config:clear
+# php artisan config:cache
+# php artisan route:cache
+# php artisan view:cache
 
-# Frontend build
-npm install
-npm run build
+# # Frontend build
+# npm install
+# npm run build
 
-# Migrate DB if needed
-php artisan migrate --force
+# # Migrate DB if needed
+# php artisan migrate --force
